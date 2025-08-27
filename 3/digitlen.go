65657800319705de9726d5bba6dbd31ff3a4a5f1
Your Name <you@example.com>
2025-08-27 13:24:51 +0500

    1

---
+++ 3/digitlen.go
@@ -0,0 +1,4 @@
+package piscine
+
+func DigitLen(n, base int) int {
+}
--- main.go
+++ main.go
@@ -2,21 +2,15 @@ package main
 
 import (
 	"fmt"
-	piscine "piscine/6"
+
+	piscine "piscine/3"
 )
 
 func main() {
-	fmt.Print(piscine.NotDecimal("0.1"))
-	fmt.Print(piscine.NotDecimal("174.2"))
-	fmt.Print(piscine.NotDecimal("0.1255"))
-	fmt.Print(piscine.NotDecimal("1.20525856"))
-	fmt.Print(piscine.NotDecimal("-0.0f00d00"))
-	fmt.Print(piscine.NotDecimal(""))
-	fmt.Print(piscine.NotDecimal("-19.525856"))
-	fmt.Print(piscine.NotDecimal("1952"))
-	fmt.Print(piscine.NotDecimal("00.02"))
-	fmt.Print(piscine.NotDecimal("51.3+95.9"))
-	fmt.Println(rune('.'))
+	fmt.Println(piscine.DigitLen(100, 10))
+	fmt.Println(piscine.DigitLen(100, 2))
+	fmt.Println(piscine.DigitLen(-100, 16))
+	fmt.Println(piscine.DigitLen(100, -1))
 }
 
 // NotDecimal("51.3+95.9") == "513+959\n" instead of "51.3+95.9\n"
